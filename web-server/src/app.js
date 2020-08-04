471d2b651ea0697113f36220e92b1ee3d4c11946
Sunil Panchal <sunilpanchal123.sp@gmail.com>
2020-08-04 14:38:50 +0530

    Express js with hbs

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,17 +1,33 @@
+const path = require('path');
 const express = require('express');
 
 const app = express();
+const publicPath = path.join(__dirname,'../public/');
+
+const viewPath = path.join(__dirname,'../template/');
+app.set('view engine','hbs');
+app.set('views',viewPath);
+app.use(express.static(publicPath));
 
 app.get('',(req,res)=>{
-    res.send('<h1>Hello express</h1>');
-})
-
-app.get('/help',(req,res)=>{
-    res.send('This is help page');
+    res.render('index',{
+        title:'Hoem page of hbs',
+        name:'Sunil Panchal'
+    })
 })
 
 app.get('/about',(req,res)=>{
-    res.send('<title>About</title>');
+    res.render('about',{
+        title:'About us in hbs',
+        name:'Sunil Panchal'
+    })
+})
+
+app.get('/help',(req,res)=>{
+    res.render('help',{
+        title:'Help page in hbs',
+        name:'Sunil panchal'
+    })
 })
 
 app.get('/weather',(req,res)=>{
