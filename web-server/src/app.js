fc5f1e2001ffeffb8d6e30379fa539fab2544061
Sunil Panchal <sunilpanchal123.sp@gmail.com>
2020-08-05 12:48:10 +0530

    partials means we drive header and footer ones and use in many applications

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,31 +1,39 @@
 const path = require('path');
 const express = require('express');
+const hbs = require('hbs')
 
 const app = express();
+//Define path for express config
 const publicPath = path.join(__dirname,'../public/');
+const viewPath = path.join(__dirname,'../template/views');
+const partialPath = path.join(__dirname,'../template/partials');
 
-const viewPath = path.join(__dirname,'../template/');
+//setup handlebars engine and views location
 app.set('view engine','hbs');
-app.set('views',viewPath);
+app.set('views', viewPath);
+hbs.registerPartials(partialPath);
+
+//setup static directory to serve
 app.use(express.static(publicPath));
+
 
 app.get('',(req,res)=>{
     res.render('index',{
-        title:'Hoem page of hbs',
+        title:'Home page',
         name:'Sunil Panchal'
     })
 })
 
 app.get('/about',(req,res)=>{
     res.render('about',{
-        title:'About us in hbs',
+        title:'About us',
         name:'Sunil Panchal'
     })
 })
 
 app.get('/help',(req,res)=>{
     res.render('help',{
-        title:'Help page in hbs',
+        title:'Help',
         name:'Sunil panchal'
     })
 })
